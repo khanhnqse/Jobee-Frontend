6ace6eee850ca5346ab0a02ef11a388c2141b3f7
khanhnqse <khanhqn03@gmail.com>
2024-11-25 02:25:48 +0700

    fixed overview

--- src/components/Bar ChartCard/BarChartCard.jsx
+++ src/components/Bar ChartCard/BarChartCard.jsx
@@ -11,22 +11,30 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-const BarChartCard = ({ title, data, backgroundColor }) => (
-  <Card title={title} style={{ backgroundColor }}>
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart
-        data={data}
-        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#3b7b7a" />
-      </BarChart>
-    </ResponsiveContainer>
-  </Card>
-);
+const BarChartCard = ({ title, data, backgroundColor }) => {
+  // Determine the dataKey dynamically based on the first data entry
+  const dataKey =
+    data.length > 0
+      ? Object.keys(data[0]).find((key) => key !== 'name')
+      : 'value';
+
+  return (
+    <Card title={title} style={{ backgroundColor }}>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey={dataKey} fill="#3b7b7a" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+};
 
 export default BarChartCard;
